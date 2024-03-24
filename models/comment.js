e64d071fcc2b7ceef5a0ea29f7618b00c32f9e72
Imran Hussain <fswebenthusiast@gmail.com>
2024-03-24 12:08:32 +0500

    Add url property to Comment model schema

--- models/comment.js
+++ models/comment.js
@@ -4,8 +4,15 @@ const { Schema } = mongoose;
 
 const CommentSchema = new Schema({
    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
-   email: { type: String, required: true },
+   email: { type: String, required: true, lowercase: true, unique: true },
    firstName: { type: String, required: true },
    lastName: { type: String, required: true },
+   comment: { type: String, required: true },
    timestamp: { type: Date, default: Date.now },
 });
+
+CommentSchema.virtual("url").get(function () {
+   return `/api/posts/comments/${this._id}`;
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
