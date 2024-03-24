f8505385639602a48ad6c473bba1b1defcdb84ec
Imran Hussain <fswebenthusiast@gmail.com>
2024-03-24 12:06:51 +0500

    Update author model schema

--- models/author.js
+++ models/author.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const AuthorSchema = new Schema({
+   firstName: { type: String, required: true },
+   lastName: { type: String, required: true },
    username: { type: String, required: true },
-   password: { type: String, required: true, length: { min: 8 } },
+   email: { type: String, required: true, unique: true },
+   password: { type: String, required: true, },
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
