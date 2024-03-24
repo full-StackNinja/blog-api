fa215e36b874fe93b9296b12a47200cf06d79e0c
Imran Hussain <fswebenthusiast@gmail.com>
2024-03-24 12:10:56 +0500

    Add url property to Post model schema

--- models/post.js
+++ models/post.js
@@ -6,7 +6,12 @@ const PostSchema = new Schema({
    author: { type: Schema.Types.ObjectId, ref: "Author", required: true },
    title: { type: String, required: true },
    description: { type: String, required: true },
+   published: { type: Boolean, default: false },
    timestamp: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("Post", PostSchema)
+PostSchema.virtual("url").get(function () {
+   return `/api/posts/${this._id}`;
+});
+
+module.exports = mongoose.model("Post", PostSchema);
